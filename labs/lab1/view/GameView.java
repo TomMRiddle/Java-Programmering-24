265ab44e79276e16317b5807b84f658f9e5c2e42
Victor Ek <victor.ek@gmail.com>
2024-10-13 23:58:35 +0200

    ensure min 2 players in multiplayer and piggame

--- labs/lab1/view/GameView.java
+++ labs/lab1/view/GameView.java
@@ -21,8 +21,8 @@ public class GameView {
     }
 
     public static String[] getPlayerNames() {
-        System.out.println("\nEnter the number of players");
-        String[] playerNames = new String[scanner.nextInt()];
+        System.out.println("\nEnter the number of players, (2 is minimum):");
+        String[] playerNames = new String[(Math.max(scanner.nextInt(),2)];
         for(int i = 0; i < playerNames.length; i++) {
             System.out.println("PLAYER " + (i + 1));
             playerNames[i] = getPlayerName();
